5c4a3e122a7839b15f4f01b65a21821311fc546f
azzykesuma <official.azzy@gmail.com>
2024-07-26 16:01:49 +0700

    moving the redux provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import { useState, createContext} from "react"
-import Layout from "./layout/Layout"
+import { useState, createContext } from "react";
+import Layout from "./layout/Layout";
 import MainForm from "./components/MainForm";
 import { StepContextType, StepType } from "Types";
-
-
+import { store } from "./store/store.ts";
+import { Provider } from "react-redux";
 
 export const StepContext = createContext<StepContextType>({
   step: 1,
-  setStep: () => {}
+  setStep: () => {},
 });
 
-
 function App() {
-  const [step, setStep] = useState<StepType>(5)
+  const [step, setStep] = useState<StepType>(5);
   return (
-  <StepContext.Provider value={{step, setStep}}>
-    <Layout>
-      <MainForm />
-    </Layout>
-  </StepContext.Provider>
-  )
+    <StepContext.Provider value={{ step, setStep }}>
+      <Layout>
+        <Provider store={store}>
+          <MainForm />
+        </Provider>
+      </Layout>
+    </StepContext.Provider>
+  );
 }
 
-export default App
+export default App;
